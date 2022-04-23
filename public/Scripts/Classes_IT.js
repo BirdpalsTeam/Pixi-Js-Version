97d56795bfd148a6ddceb982e950c384728b8aed
Gasegamer <66225865+Gasegamer@users.noreply.github.com>
2022-04-23 17:17:23 -0300

    Added flines' items.

--- public/Scripts/Classes_IT.js
+++ public/Scripts/Classes_IT.js
@@ -1,19 +1,23 @@
-class Item extends PIXI.Sprite{
-	constructor(item, frame){
-		super(resources.items.textures[`${item}_4.png`]);
-		this.anchor.set(0.5, -0.5);
-		this.item = item;
-		this.frame = frame;
-		this.itemData = resources.items.data;
-		this.y = this.itemData.frames[`${item}_${frame}.png`].position.y;
-		this.x = this.itemData.frames[`${item}_${frame}.png`].position.x;
-	}
+class Item extends PIXI.Sprite {
+  constructor(item, frame) {
+    super(resources.items.textures[`${item}_4.png`]);
+    this.anchor.set(0.5, -0.5);
+    this.item = item;
+    this.frame = frame;
+    this.itemData = resources.items.data;
+    this.y = this.itemData.frames[`${item}_${frame}.png`].position.y;
+    this.x = this.itemData.frames[`${item}_${frame}.png`].position.x;
+  }
 
-	updateFrame(frame){
-		this.texture = resources.items.textures[`${this.item}_${frame}.png`];
-		this.y = this.itemData.frames[`${this.item}_${frame}.png`].position.y;
-		this.x = this.itemData.frames[`${this.item}_${frame}.png`].position.x;
-
-		this.frame = frame; //For debugging
-	}
+  updateFrame(frame) {
+    if (`${this.item}_${frame}.png` in resources.items.textures) {
+      if (this.visible === false) {this.visible = true}
+      this.texture = resources.items.textures[`${this.item}_${frame}.png`];
+      this.y = this.itemData.frames[`${this.item}_${frame}.png`].position.y;
+      this.x = this.itemData.frames[`${this.item}_${frame}.png`].position.x;
+    }else{
+      this.visible = false;
+    }
+    this.frame = frame;
+  }
 }
