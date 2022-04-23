a07b412422fb6e8213a0c5609a566705de457be7
Gasegamer <66225865+Gasegamer@users.noreply.github.com>
2022-04-23 19:49:52 -0300

    Started new login page

--- public/Scripts/login.js
+++ public/Scripts/login.js
@@ -4,7 +4,7 @@ loginButton.addEventListener('click', logIn);
 createAccountButton.addEventListener('click', createAccount);
 function logIn() {
 	var eMail = document.getElementById('input-e');
-	var password = document.getElementById('input-p'); // Feel free to add a function that the password input.value appear as ***** to the player
+	var password = document.getElementById('input-p');
 		PlayFabClientSDK.LoginWithEmailAddress(			//Log in directly to Playfab
 			{
 			TitleId: "238E6",
@@ -46,7 +46,7 @@ function createAccount(){
 }
 $(document).ready(function() {
 
-	$('.submit_on_enter').keydown(function(event) {
+	$('.input').keydown(function(event) {
 	  // enter has keyCode = 13, change it if you want to use another button
 	  if (event.keyCode == 13) {
 		logIn();
